960aea2ec03d04e59f7432e8195a4ebc6c096e16
HYUNJUNE <june2ac4dev@gmail.com>
2024-05-31 15:10:35 +0900

    Res:drawable, string res 추가

--- app/src/main/java/com/aos/goodideacard/consts/AppConst.kt
+++ app/src/main/java/com/aos/goodideacard/consts/AppConst.kt
@@ -4,5 +4,5 @@ object AppConst {
     const val LOG_TAG_LIFE_CYCLE = "debugLog_LifeCycle"
     const val LOG_TAG_VIEW_MODEL = "debugLog_ViewModel"
 
-    const val TOTAL_CARD_50 = 16
+    const val TOTAL_CARD_50 = 22
 }
