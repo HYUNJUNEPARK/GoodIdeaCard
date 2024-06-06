495fe4697eccbeca707060c3d274c256ce3775d5
HYUNJUNE <june2ac4dev@gmail.com>
2024-06-07 08:33:23 +0900

    Design:Language UI 추가

---
+++ app/src/main/java/com/aos/goodideacard/enums/Language.kt
@@ -0,0 +1,7 @@
+package com.aos.goodideacard.enums
+
+enum class Language(val value:String) {
+    DEFAULT(""),
+    KOREAN("ko"),
+    ENGLISH("en")
+}
--- app/src/main/java/com/aos/goodideacard/features/setting/LanguageFragment.kt
+++ app/src/main/java/com/aos/goodideacard/features/setting/LanguageFragment.kt
@@ -5,8 +5,15 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.navigation.fragment.findNavController
+import com.aos.goodideacard.R
 import com.aos.goodideacard.databinding.FragmentLanguageBinding
+import com.aos.goodideacard.enums.Language
 import com.aos.goodideacard.features.base.BaseFragment
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import timber.log.Timber
 
 class LanguageFragment : BaseFragment() {
     private var _binding: FragmentLanguageBinding? = null
@@ -20,10 +27,47 @@ class LanguageFragment : BaseFragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        //checkUserSettingOnRadioButton()
     }
 
     override fun onDestroy() {
         super.onDestroy()
         _binding = null
     }
+
+//    private fun checkUserSettingOnRadioButton() = CoroutineScope(Dispatchers.IO).launch {
+//        val language = appDataStoreManager.getLanguage()
+//
+//        withContext(Dispatchers.Main) {
+//            when(language) {
+//                Language.DEFAULT -> binding.languageBtnSystemLanguage.isChecked = true
+//                Language.KOREAN -> binding.languageBtnKorean.isChecked = true
+//                Language.ENGLISH -> binding.languageBtnEnglish.isChecked = true
+//            }
+//            onRadioButtonClicked()
+//        }
+//    }
+//
+//    private fun onRadioButtonClicked() {
+//        binding.languageRadioGroup.setOnCheckedChangeListener { /*radioGroup*/_, id ->
+//            val language = when(id) {
+//                R.id.language_btn_system_language -> Language.DEFAULT
+//                R.id.language_btn_korean -> Language.KOREAN
+//                R.id.language_btn_english -> Language.ENGLISH
+//                else -> {
+//                    Timber.e("Exception:Not handling language btn id($id")
+//                    Language.DEFAULT
+//                }
+//            }
+//
+//            CoroutineScope(Dispatchers.IO).launch {
+//                appDataStoreManager.saveLanguage(language)
+//
+//                withContext(Dispatchers.Main) {
+//                    (requireActivity() as MainActivity).changeLanguage(language)
+//                }
+//            }
+//        }
+//    }
 }
