a8760ba9fd2e647e4d2465682dcb0cafbc0a1a12
HYUNJUNE PARK <june2ac4dev@gmail.com>
2024-06-22 22:26:59 +0900

    Feat:appy adapterCardList

--- app/src/main/java/com/aos/goodideacard/features/card/CardAddFragment.kt
+++ app/src/main/java/com/aos/goodideacard/features/card/CardAddFragment.kt
@@ -1,9 +1,11 @@
 package com.aos.goodideacard.features.card
 
+import android.graphics.Rect
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.ViewTreeObserver
 import android.widget.Toast
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.lifecycleScope
@@ -48,6 +50,7 @@ class CardAddFragment: BaseFragment() {
         viewModel.getCards(cardPackId!!)
 
         initListener()
+
 
         lifecycleScope.launch {
             viewModel.cards.collect { cards ->
